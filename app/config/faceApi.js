16d752e2ad5943033222a792bf0e2e1e38c5ab31
Kayque Agape <k1099gm@gmail.com>
2026-01-26 22:19:00 -0300

    feat: add swagger

---
+++ app/config/faceApi.js
@@ -0,0 +1,5 @@
+// Face API configuration and model loading
+// This file ensures face recognition models are loaded correctly
+// Models are loaded in app/utils/faceRecognition.js
+
+export const FACE_MODELS_PATH = './app/models/faceid';
